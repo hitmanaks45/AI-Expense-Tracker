42cf023ee17d1eab49f231b3f5db7c8321ea4c2b
hitmanaks45 <ayushsingh28sep@gmail.com>
2026-07-05 02:13:46 +0530

    Fix CORS for Vercel

--- Backend/server.js
+++ Backend/server.js
@@ -26,7 +26,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://ai-expense-tracker-gold-ten.vercel.app",
+    ],
     credentials: true,
   })
 );
