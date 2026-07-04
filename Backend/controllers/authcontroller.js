d88c64592c4d31f77e966de04ef7cf9ed50b79be
hitmanaks45 <ayushsingh28sep@gmail.com>
2026-07-05 01:30:30 +0530

    Rename TempUser to User

--- Backend/controllers/authcontroller.js
+++ Backend/controllers/authcontroller.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcryptjs");
-const User = require("../models/TempUser");
+const User = require("../models/User");
 const generateToken = require("../utils/generateToken");
 
 // =================== GOOGLE CALLBACK ===================
--- Backend/services/userService.js
+++ Backend/services/userService.js
@@ -1,4 +1,4 @@
-const User = require("../models/TempUser");
+const User = require("../models/User");
 
 const getCurrentUser = async (userId) => {
   return await User.findById(userId).select("-__v");
